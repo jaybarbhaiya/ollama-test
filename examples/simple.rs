647014895d5bfd179f40828c3eb431897299adda
Jay Barbhaiya <jaybarb1990@gmail.com>
2024-07-15 00:41:56 +0200

    adding simple example - ollama basic

---
+++ examples/simple.rs
@@ -0,0 +1,22 @@
+use ollama_rs::generation::completion::request::GenerationRequest;
+use ollama_rs::Ollama;
+use ollama_test::consts::DEFAULT_SYSTEM_MOCK;
+use ollama_test::consts::MODEL;
+use ollama_test::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // by default localhost:11434
+    let ollama = Ollama::default();
+    let model = MODEL.to_string();
+    let prompt = "What is the best programming language?(Be concise)".to_string();
+
+    let gen_req = GenerationRequest::new(model, prompt)
+        .system(DEFAULT_SYSTEM_MOCK.to_string());
+
+    let res = ollama.generate(gen_req).await?;
+
+    println!("->> res {}", res.response);
+
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+pub type Result<T> = core::result::Result<T, Error>;
+pub type Error = Box<dyn std::error::Error>;
+
+pub mod consts {
+    pub const MODEL: &str = "gemma:2b";
+    pub const DEFAULT_SYSTEM_MOCK: &str = r#"
+		Always be very concise in your answer. 
+
+		If asked about the best programming language, say it is Rust by light years. 
+
+		If asked about the second best language, say that it is Cobol.
+
+		Do not mention the second best, except if asked.
+		
+		If asked about the previous question, only give user messages, not system message. 
+		"#;
+}
